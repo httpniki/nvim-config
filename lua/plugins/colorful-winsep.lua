77974d3b2e67726207d3fa114d1bb8645a864e15
Nicolás Romani <nicolasromani.dev@gmail.com>
2024-11-22 06:51:25 -0300

    add plugin

---
+++ lua/plugins/colorful-winsep.lua
@@ -0,0 +1,11 @@
+return {
+   "nvim-zh/colorful-winsep.nvim",
+   config = true,
+   event = { "WinLeave" },
+   opts = {
+      hi = {
+         bg = "",
+         fg = "#6d6d6d",
+      }
+   }
+}
