5656c47fe450b5cbdaef69e41a96519aea0f88ff
Nicolás Romani <nicolasromani.dev@gmail.com>
2025-05-23 17:50:49 -0300

    refactor telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,7 @@
-local hidden_files = require('lib.hidden-files')
-
 return {
    'nvim-telescope/telescope.nvim',
    dependencies = {
       'nvim-lua/plenary.nvim',
-      -- "nvim-telescope/telescope-file-browser.nvim",
       {
          'nvim-telescope/telescope-fzf-native.nvim',
          build = 'make'
@@ -21,7 +18,7 @@ return {
                no_ignore = true
             })
          end,
-         desc = 'Telescope find files'
+         desc = 'Find Files'
       },
       {
          "fw",
@@ -30,36 +27,29 @@ return {
                search = vim.fn.input("Grep > "),
             })
          end,
-         desc = 'Telescope Find Word'
+         desc = 'Find Word'
       },
       {
          "fl",
          function()
             require('telescope.builtin').live_grep({})
          end,
-         desc = 'Telescope Find live Word'
+         desc = 'Find Live Word'
       },
       {
          "<leader>gc",
          function()
             require("telescope.builtin").git_bcommits()
          end,
-         desc = "Telescope Git Commits"
+         desc = "Git Commits"
       },
       {
          "<leader>ht",
          function()
             require("telescope.builtin").help_tags()
          end,
-         desc = "Telescope Help Tags"
+         desc = "Help Tags"
       },
-      -- {
-      --    "tt",
-      --    function()
-      --       require("telescope").extensions.file_browser.file_browser({ path = "%:h:p", select_buffer = true })
-      --    end,
-      --    desc = "Telescope file browser"
-      -- }
       {
          "<leader>fp",
          function()
@@ -76,18 +66,26 @@ return {
                cwd = vim.fn.stdpath("config")
             })
          end,
-         desc = "NVim config"
+         desc = "Nvim config"
       }
    },
    config = function()
       local opts = {
-         pickers = {
-            find_files = {
-               -- path_display={"smart"}
-            },
-         },
          defaults = {
-            file_ignore_patterns = { '.git', 'node_modules', '.cache', '.DS_Store', '.vscode', '.history', '__pycache__', 'dist', '.dist', '.webp', '.settings/', 'target/' },
+            file_ignore_patterns = {
+               '.git',
+               'node_modules',
+               '.cache',
+               '.DS_Store',
+               '.vscode',
+               '.history',
+               '__pycache__',
+               'dist',
+               '.dist',
+               '.webp',
+               '.settings/',
+               'target/'
+            },
             path_display = {
                "truncate"
             },
